e326a2d5c5d190163a73dd3e1f1af1840b03c15e
hoangbap010595 <lchoang1995@gmail.com>
2017-03-18 21:55:22 +0700

    Create package config_app  18-3-2017 Hoang

---
+++ GameVHTA/app/src/main/java/blackcore/tdc/edu/com/gamevhta/config_app/ConfigApplication.java
@@ -0,0 +1,35 @@
+package blackcore.tdc.edu.com.gamevhta.config_app;
+
+/**
+ * Created by HOANG on 3/18/2017.
+ */
+
+public class ConfigApplication {
+
+    /*------------- START HOANG -----------------*/
+    //Time left default
+    public static final int TIME_LEFT_GAME = 50;
+
+    //Database
+    public static final String DATABASE_NAME = "GAMEVHTA";
+
+    //TABLE
+    public static final String TABLE_WORD = "TABLE_WORD";
+    public static final String TABLE_SCORE = "TABLE_SCORE";
+
+    //COLUMN
+    //TABLE WORD
+    public static final String W_COLUMN_ID_WORD = "ID_WORD";
+    public static final String W_COLUMN_WORD_ENG = "WORD_ENG";
+    public static final String W_COLUMN_WORD_VIE = "WORD_VIE";
+    public static final String W_COLUMN_PATH_IMAGE = "PATH_IMAGE";
+    public static final String W_COLUMN_PATH_SOUND = "PATH_SOUND";
+    public static final String W_COLUMN_OBJECT = "OBJECT";
+
+    //TABLE SCORE
+    public static final String S_COLUMN_ID_PLAYER = "ID_PLAYER";
+    public static final String S_COLUMN_PLAYER_NAME = "PLAYER_NAME";
+    public static final String S_COLUMN_PLAYER_SCORE = "PLAYER_SCORE";
+
+    /*------------- END HOANG -----------------*/
+}
