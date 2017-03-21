95c67353f08c85421705bb697ab6a2dcde8ead72
hoangbap010595 <lchoang1995@gmail.com>
2017-03-21 13:54:07 +0700

    Update code 21-3-2017 by Hoang

--- GameVHTA/app/src/main/java/blackcore/tdc/edu/com/gamevhta/my_adapter/AdapterScore.java
+++ GameVHTA/app/src/main/java/blackcore/tdc/edu/com/gamevhta/my_adapter/AdapterScore.java
@@ -1,39 +1,47 @@
 package blackcore.tdc.edu.com.gamevhta.my_adapter;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
 import blackcore.tdc.edu.com.gamevhta.R;
-import blackcore.tdc.edu.com.gamevhta.models.Score;
+import blackcore.tdc.edu.com.gamevhta.models.ScoreObject;
 
 /**
  * Created by phong on 2/27/2017.
  */
 
-public class AdapterScore extends ArrayAdapter<Score>{
+public class AdapterScore extends ArrayAdapter<ScoreObject> {
     private Activity context;
     private int idLayout;
-    private ArrayList<Score> arr;
-    public AdapterScore(Activity context, int idLayout, ArrayList<Score> arr) {
-        super(context, idLayout, arr);
+
+    public AdapterScore(Activity context, int idLayout) {
+        super(context, idLayout);
         this.context = context;
         this.idLayout = idLayout;
-        this.arr = arr;
     }
-    public View getView(final int position, View convertView, ViewGroup parent){
-            convertView = this.context.getLayoutInflater().inflate(idLayout,null);
+
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        convertView = this.context.getLayoutInflater().inflate(idLayout, null);
         ImageView imvMedal = (ImageView) convertView.findViewById(R.id.imgMedal);
         TextView txtName = (TextView) convertView.findViewById(R.id.txtName);
         TextView txtScore = (TextView) convertView.findViewById(R.id.txtScore);
-        imvMedal.setImageResource(R.drawable.medal);
-        txtName.setText(arr.get(position).getName());
-        txtScore.setText(arr.get(position).getScore());
+        if(position<3)
+            imvMedal.setImageResource(R.drawable.medal);
+        else
+            imvMedal = null;
+        ScoreObject scoreObject = getItem(position);
+        txtName.setText(scoreObject.getsPlayer());
+        txtScore.setText(String.valueOf(scoreObject.getsScore()));
+        Log.d("ScorePos", String.valueOf(position));
+        Log.d("ScorePlay", scoreObject.getsPlayer());
+        Log.d("ScoreInt", String.valueOf(scoreObject.getsScore()));
+        Log.d("ScoreID", scoreObject.getsID());
+
         return convertView;
     }
 }
