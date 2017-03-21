0d5c38a98cb6538077d5c9e96fbae8357e181845
hoangbap010595 <lchoang1995@gmail.com>
2017-03-21 13:48:58 +0700

    Update file config 21-3-2017 by Hoang

--- GameVHTA/app/src/main/java/blackcore/tdc/edu/com/gamevhta/config_app/ConfigApplication.java
+++ GameVHTA/app/src/main/java/blackcore/tdc/edu/com/gamevhta/config_app/ConfigApplication.java
@@ -10,9 +10,23 @@ public class ConfigApplication {
     //Time left default
     public static final int TIME_LEFT_GAME = 50;
 
+
+    //Object
+    public static final String OBJECT_SELECTED = "SCH";
+    //School
+    public static final String OBJECT_SCHOOL = "SCH";
+    //Animals
+    public static final String OBJECT_ANIMALS = "ANI";
+    //Plants
+    public static final String OBJECT_PLANTS = "PLA";
+    //CountrySide
+    public static final String OBJECT_COUNTRY_SIDE = "COU";
+    //Household Appliances
+    public static final String OBJECT_HOUSEHOLD_APPLIANCES = "HOU";
+
     //Database
     public static final String DATABASE_NAME = "GAMEVHTA";
-
+    public static final int DATABASE_VERSION = 1;
     //TABLE
     public static final String TABLE_WORD = "TABLE_WORD";
     public static final String TABLE_SCORE = "TABLE_SCORE";
@@ -25,6 +39,7 @@ public class ConfigApplication {
     public static final String W_COLUMN_PATH_IMAGE = "PATH_IMAGE";
     public static final String W_COLUMN_PATH_SOUND = "PATH_SOUND";
     public static final String W_COLUMN_OBJECT = "OBJECT";
+    public static final String W_COLUMN_LEVEL = "LEVEL";
 
     //TABLE SCORE
     public static final String S_COLUMN_ID_PLAYER = "ID_PLAYER";
